c5bc7a6c755d37ec08c839a6e1528c5ba0e5fd18
Tyler Haun <frodo03211@gmail.com>
2018-02-19 21:02:09 +0000

    Remove padding from parent element in response view showing undesired white element on calendar

--- dst/vendor/abl-sdk.vendor.js
+++ dst/vendor/abl-sdk.vendor.js
@@ -90,7 +90,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "11b347592c8f5a7cd4ea"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "e1a8a6ecab27f8a3e2df"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
