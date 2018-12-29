7cd718b3570bff398660c33cb661bc9ec2331744
Gonzalo Geraldo <geraldo.gonzalo@gmail.com>
2018-12-29 05:19:50 +0000

    Fix class assigned in the wrong element

--- dst/vendor/abl-sdk.vendor.js
+++ dst/vendor/abl-sdk.vendor.js
@@ -90,7 +90,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "1b2c41ef8c41d43fdeb3"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "6702cdf4d2d3022e1db9"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
